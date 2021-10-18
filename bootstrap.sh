08b6a88ef1eef9fa2b5ed9cba63eeec27b48a6a8
Spencer Haan <spencer.haan+github@shopify.com>
2021-10-18 13:14:16 -0400

    Added Bash alieses to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,3 +10,7 @@ git config --global alias.co checkout
 git config --global alias.b branch
 git config --global alias.c commit
 git config --global alias.pfp '!git pull && git fetch --prune'
+
+# Setup Bash aliases
+echo "alias ll='ls -l'" >> ~/.zshrc
+echo "alias la='ls -la'" >> ~/.zshrc
