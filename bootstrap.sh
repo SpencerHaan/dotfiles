f2be206cfeabb00caf3a0c729da3cacd3957e5e9
Spencer Haan <spencer.haan+github@shopify.com>
2021-10-18 11:57:12 -0400

    Added bootstrap.sh for Spin

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+########################################################################################################################
+#
+# Bootstrap script for Shopfiy Spin
+#
+########################################################################################################################
+
+
+# Git Setup
+git config --global alias.co checkout
+git config --global alias.b branch
+git config --global alias.c commit
+git config --global alias.pfp '!git pull && git fetch --prune'
